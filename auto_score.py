85a3cb2b61b595cee85dd1368889c21e7d7a44fb
마스터 <dnjswo105@naver.com>
2022-07-28 06:07:49 +0000

    auto score system operate

--- auto_score.py
+++ auto_score.py
@@ -6,7 +6,7 @@ jwj_swea = os.listdir('jwj/swea/D1-D2')
 
 
 with open('/workspace/sw-algorithm/score.txt', 'w', encoding='utf-8') as make_file:
-    make_file.write(f'정원재\n백준 해결한 문제 : {len(jwj_baek)}\nSW Expert Academy 해결한 문제 : {len(jwj_swea)}')
+    make_file.write(f'정원재\n백준 해결한 문제 : {len(jwj_baek)}\nSW Expert Academy 해결한 문제 : {len(jwj_swea)}\n총 해결한 문제{len(jwj_baek) + len(jwj_swea)}')
     
 
 os.system('git pull')
