ce54ded15e1670d6d33b70d9e9d2ff36342f85cd
cyg <cyellowgreen@gmail.com>
2022-08-07 09:10:54 +0900

    1018 단축버전 추가

--- cyk/solved_ac/class2/1018.py
+++ cyk/solved_ac/class2/1018.py
@@ -1,7 +1,54 @@
+# n, k = map(int, input().split())
+# result =[]
+# count_1_result =[]
+# count_2_result =[]
+# for _ in range(n):
+#     a = list(input())
+#     result.append(a)
+
+# for x in range(0, k-7):
+#     for y in range(0,n-7) :
+#         count_1 = 0
+#         count_2 = 0
+#         slice_chess =[]
+#         for i in range(y, y+8) :
+#             slice_chess.append(result[i][x:x+8])
+#         for idx_1, lst in enumerate(slice_chess):
+#             if not(idx_1 % 2) :
+#                 for idx_2, elem in enumerate(lst) :
+#                     if idx_2 % 2 and elem == 'B':
+#                         count_1 += 1
+#                     if not(idx_2 % 2) and elem == 'W':
+#                         count_1 += 1 
+#             else :
+#                 for idx_2 ,elem in enumerate(lst):
+#                     if idx_2 % 2 and elem == 'W' :
+#                         count_1 += 1
+#                     if not(idx_2 % 2) and elem == 'B':
+#                         count_1 += 1
+
+#         for idx_1, lst in enumerate(slice_chess):
+#             if not(idx_1 % 2) :
+#                 for idx_2, elem in enumerate(lst) :
+#                     if idx_2 % 2 and elem == 'W':
+#                         count_2 += 1
+#                     if not(idx_2 % 2) and elem == 'B':
+#                         count_2 += 1 
+#             else :
+#                 for idx_2 ,elem in enumerate(lst):
+#                     if idx_2 % 2 and elem == 'B' :
+#                         count_2 += 1
+#                     if not(idx_2 % 2) and elem == 'W':
+#                         count_2 += 1
+                                                        
+#         count_1_result.append(count_1)
+#         count_2_result.append(count_2)
+# print(min(min(count_1_result), min(count_2_result)))    
+
+
 n, k = map(int, input().split())
 result =[]
 count_1_result =[]
-count_2_result =[]
 for _ in range(n):
     a = list(input())
     result.append(a)
@@ -9,7 +56,6 @@ for _ in range(n):
 for x in range(0, k-7):
     for y in range(0,n-7) :
         count_1 = 0
-        count_2 = 0
         slice_chess =[]
         for i in range(y, y+8) :
             slice_chess.append(result[i][x:x+8])
@@ -27,22 +73,6 @@ for x in range(0, k-7):
                     if not(idx_2 % 2) and elem == 'B':
                         count_1 += 1
 
-        for idx_1, lst in enumerate(slice_chess):
-            if not(idx_1 % 2) :
-                for idx_2, elem in enumerate(lst) :
-                    if idx_2 % 2 and elem == 'W':
-                        count_2 += 1
-                    if not(idx_2 % 2) and elem == 'B':
-                        count_2 += 1 
-            else :
-                for idx_2 ,elem in enumerate(lst):
-                    if idx_2 % 2 and elem == 'B' :
-                        count_2 += 1
-                    if not(idx_2 % 2) and elem == 'W':
-                        count_2 += 1
-                                                        
         count_1_result.append(count_1)
-        count_2_result.append(count_2)
-print(min(min(count_1_result), min(count_2_result)))    
-
-    
+        count_1_result.append(64-count_1) # W로 시작하는 경우
+print(min(count_1_result))
