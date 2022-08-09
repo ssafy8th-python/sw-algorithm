d088f1dac3ea2c276f11c778241999634a0f7f21
cyg <cyellowgreen@gmail.com>
2022-08-10 00:47:07 +0900

    11651

--- cyk/solved_ac/class2/11651.py
+++ cyk/solved_ac/class2/11651.py
@@ -1,8 +1,24 @@
 N = int(input())
 
-lst = [list(map(int,input().split())) for _ in range(N)]
+# lst = [list(map(int,input().split())) for _ in range(N)]
 
-print(sorted(lst, key=lambda x :x[1]))
+# sorted_lst = sorted(lst, key=lambda x :x[1])
 
-for k in sorted(lst, key=lambda x :x[1]) :
-    if 
+# for num in range(N-1):
+#     if sorted_lst[num][1] == sorted_lst[num+1][1]:
+#         if sorted_lst[num][0] > sorted_lst[num][1] :
+#             sorted_lst[num], sorted_lst[num+1] = sorted_lst[num+1], sorted_lst[num]
+
+# for elem in sorted_lst :
+#     print(elem[0], elem[1])
+
+# FAIL
+
+lst = []
+
+for _ in range(N) :
+    x, y = map(int, input().split())
+    lst.append([y,x])
+
+for elem in sorted(lst) :
+    print(elem[1], elem[0])
