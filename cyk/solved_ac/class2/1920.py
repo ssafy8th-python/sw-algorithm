aa0d7b790de5ccf625eb0d2610748d2aa97d9be5
cyg <cyellowgreen@gmail.com>
2022-08-11 19:23:54 +0900

    수정

--- cyk/solved_ac/class2/1920.py
+++ cyk/solved_ac/class2/1920.py
@@ -1,11 +1,49 @@
-import sys
-input = sys.stdin.readline
+'''
+1 ≤ N ≤ 100,000
 
+입력
+5
+4 1 5 2 3
+
+5
+1 3 7 9 5
+
+출력
+1
+1
+0
+0
+1
+'''
+
+
+
+# 오답 - 시간초과
+# import sys
+# input = sys.stdin.readline
+#
+# N = int(input())
+# A_lst = list(map(int, input().split()))
+#
+# M = int(input())
+# M_lst = list(map(int, input().split()))
+#
+# x  = list(set(A_lst)&set(M_lst)) [1, 3, 5]
+#
+# for elem in M_lst :
+#     if x.count(elem) == 0 :
+#         print(0)
+#     else :
+#         print(1)
+
+import sys
+
+input = sys.stdin.readline
 
 N = int(input())
 a = (map(int, input().split()))
 
-dict_a = {x : 1 for x in a}
+dict_a = {x : 1 for x in a} # {4:1, 1:1, 5:1, 2:1, 3:1}
 
 M = int(input())
 m = (map(int, input().split()))
@@ -16,4 +54,6 @@ for k in m :
         print(1)
     else :
         print(0)
+
+
 
--- cyk/solved_ac/class2/2164.py
+++ cyk/solved_ac/class2/2164.py
@@ -1,10 +1,13 @@
 N = int(input())
-
 lst = list(range(1,N+1))
+count = 0
 
-
-
-while len(lst)>1 :
-    lst[2:].append(lst[1])
-    
+while len(lst) > 1:
+    if count % 2:
+        a = lst.pop(0)
+        lst.append(a)
+        count += 1
+    else:
+        lst.pop(0)
+        count += 1
 print(lst[0])
