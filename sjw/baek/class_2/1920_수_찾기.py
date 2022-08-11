c647da9c8c7969745f848d9ad5e32d90aa032da3
appletail <csappletail@gmail.com>
2022-08-12 01:05:24 +0900

    수 찾기 재풀이

--- sjw/baek/class_2/1920_수_찾기.py
+++ sjw/baek/class_2/1920_수_찾기.py
@@ -2,28 +2,42 @@
 # 주소: https://www.acmicpc.net/problem/1920
 
 # 제출한 답
-import sys
-input = sys.stdin.readline
 
+# import sys
+# input = sys.stdin.readline
+
+# def binary(arr, n, key):
+#     start = 0
+#     end = n - 1
+
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if key == arr[mid]:
+#             return 1
+#         elif key < arr[mid]:
+#             end = mid - 1
+#         elif key > arr[mid]:
+#             start = mid + 1
+#     return 0
+
+# N = int(input())
+# N_lst = list(map(int, input().split()))
+# M = int(input())
+# M_lst = list(map(int, input().split()))
+
+# N_lst.sort()
+
+# for i in M_lst:
+#     print(binary(N_lst, N, i))
+
+# 다른 풀이
 N = int(input())
-N_lst = list(map(int, input().split()))
+N_set = set(map(int, input().split()))
 M = int(input())
 M_lst = list(map(int, input().split()))
 
-N_lst.sort()
-
 for i in M_lst:
-    start = 0
-    end = N - 1
-    while True:
-        mid = (start + end) // 2
-        if start > end:
-            print(0)
-            break
-        elif i == N_lst[mid]:
-            print(1)
-            break
-        elif i < N_lst[mid]:
-            end = mid - 1
-        elif i > N_lst[mid]:
-            start = mid + 1
+    if i in N_set:
+        print(1)
+    else:
+        print(0)
