723419658f70a09ca7479c65450270e1dd4345a8
appletail <csappletail@gmail.com>
2022-08-11 20:58:59 +0900

    gitignore

--- sjw/baek/class_2/1920_수_찾기.py
+++ sjw/baek/class_2/1920_수_찾기.py
@@ -1,8 +1,7 @@
 # 1920 수 찾기
 # 주소: https://www.acmicpc.net/problem/1920
 
-# 제출한 답 5 2 3
-#          4 2 2
+# 제출한 답
 import sys
 input = sys.stdin.readline
 
@@ -15,16 +14,16 @@ N_lst.sort()
 
 for i in M_lst:
     start = 0
-    end = N
-
+    end = N - 1
     while True:
-        if i == N_lst[(end - start)//2]:
-            print(1)
-            break
-        elif start > end:
+        mid = (start + end) // 2
+        if start > end:
             print(0)
             break
-        elif i < N_lst[(end - start)//2] and end >= start:
-            end = (end - start)//2 - 1
-        elif i > N_lst[(end - start)//2] and end >= start:
-            start = (end - start)//2 + 1
+        elif i == N_lst[mid]:
+            print(1)
+            break
+        elif i < N_lst[mid]:
+            end = mid - 1
+        elif i > N_lst[mid]:
+            start = mid + 1
