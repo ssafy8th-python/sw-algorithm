6c973ac8bae2ae14b62b1c426d6acc0eb0ec1720
cyg <cyellowgreen@gmail.com>
2022-08-13 21:10:10 +0900

    class2 9012

--- cyk/solved_ac/class2/9012.py
+++ cyk/solved_ac/class2/9012.py
@@ -1,1 +1,46 @@
-a = '()'
+# 6
+# (())())
+# (((()())()
+# (()())((()))
+# ((()()(()))(((())))()
+# ()()()()(()()())()
+# (()((())()(
+
+T = int(input())
+for _ in range(T):
+    a = list(input())
+    N = len(a)-1
+    play = True
+    while play:
+        for i in range(N):
+            if a[i] == '(' and a[i+1] == ')':
+                a.pop(i)
+                a.pop(i)
+                N = len(a) -1
+                break
+        else:
+            print('NO')
+            play = False
+
+        if len(a) == 0:
+            print('YES')
+            play = False
+
+
+# a = list('(()())((()))')
+# N = len(a) - 1
+# play = True
+# while play:
+#     for i in range(N):
+#         if a[i] == '(' and a[i + 1] == ')':
+#             a.pop(i)
+#             a.pop(i)
+#             N = len(a) - 1
+#             break
+#     else:
+#         print('No')
+#         break
+#
+#     if len(a) == 0:
+#         print('Yes')
+#         play = False
