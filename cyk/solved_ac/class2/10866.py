8f5b18298645624e4bb775f23475de1c53710fea
cyg <cyellowgreen@gmail.com>
2022-08-14 12:45:34 +0900

    0814 class2 10866

--- cyk/solved_ac/class2/10866.py
+++ cyk/solved_ac/class2/10866.py
@@ -1,6 +1,45 @@
-N, K = map(int, input().split())
+# 덱 구현하기
+import sys
+input = sys.stdin.readline
 
-test_dict = {x:1 for x in range(1,N+1)}
+result = []
+def deque(lst):
+    if lst[0] == 'push_front':
+        result.insert(0,int(lst[1]))
+    if lst[0] == 'push_back':
+        result.append(int(lst[1]))
+    if lst[0] == 'pop_front':
+        if result: # result에 값이 있으면
+            a = result.pop(0)
+            print(a)
+        else:
+            print(-1)
+    if lst[0] == 'pop_back':
+        if result:  # result에 값이 있으면
+            a = result.pop(len(result)-1)
+            print(a)
+        else:
+            print(-1)
+    if lst[0] == 'size':
+        print(len(result))
+    if lst[0] == 'empty':
+        if result:
+            print(0)
+        else:
+            print(1)
+    if lst[0] == 'front':
+        if result:
+            print(result[0])
+        else:
+            print(-1)
+    if lst[0] == 'back':
+        if result:
+            print(result[len(result)-1])
+        else:
+            print(-1)
 
-for check in range(K) :
-            
+
+n = int(input())
+for _ in range(n):
+    s_input = input().split()
+    deque(s_input)
