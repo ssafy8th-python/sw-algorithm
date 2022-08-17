2efbc5f4b5a876dd805cd122370bf671abe9fd53
cyg <cyellowgreen@gmail.com>
2022-08-17 21:15:25 +0900

    class2++ 9012 풀이 추가

--- cyk/solved_ac/class2/9012.py
+++ cyk/solved_ac/class2/9012.py
@@ -27,20 +27,24 @@ for _ in range(T):
             play = False
 
 
-# a = list('(()())((()))')
-# N = len(a) - 1
-# play = True
-# while play:
-#     for i in range(N):
-#         if a[i] == '(' and a[i + 1] == ')':
-#             a.pop(i)
-#             a.pop(i)
-#             N = len(a) - 1
-#             break
-#     else:
-#         print('No')
-#         break
-#
-#     if len(a) == 0:
-#         print('Yes')
-#         play = False
+# stack
+
+T = int(input())
+for _ in range(T):
+    a = list(input())
+    st = []
+    for i in a:
+        if i == '(':
+          st.append(i)
+        else:
+            if st:
+                st.pop()
+            else:
+                st.append(i)
+                break
+
+    if st:
+        print('NO')
+    else:
+        print('YES')
+
