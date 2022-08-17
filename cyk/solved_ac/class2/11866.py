58cd530077b49a08da2f6fa986c9a3e0467ab233
cyg <cyellowgreen@gmail.com>
2022-08-17 21:16:11 +0900

    class2++ 11866설명추가

--- cyk/solved_ac/class2/11866.py
+++ cyk/solved_ac/class2/11866.py
@@ -1,6 +1,26 @@
 # 요세푸스
-N, K = map(int, input().split())
-lst = list(range(1,N+1))
+N, K = map(int, input().split())    # N = 7, k = 3
+lst = list(range(1,N+1))            # [1, 2, 3, 4, 5, 6, 7]
+result = []
+
+while lst:                          # lst에 요소가 없다면 while 끝
+    for _ in range(K-1):            # k번째의 요소를 없애햐 하기 때문에 k-1번 반복하여 앞의 요소들을 뒤로 보냄
+        a = lst.pop(0)
+        lst.append(a)
+    b = lst.pop(0)                  # k번째 요소를 제거하여 b에 할당
+    result.append(b)                # 제거된 요소들의 집합
+
+print('<'+ ', '.join(map(str, result))+'>')     # 제거된 순서대로 출력 <3, 6, 2, 7, 5, 1, 4>
+
+
+
+
+
+
+
+
+
+
 
 # class Queue:
 #     def __init__(self):
@@ -15,12 +35,3 @@ lst = list(range(1,N+1))
 #             x = self.items[self.front_index]
 #             self.front_index += 1
 #             return x
-result = []
-while lst :
-    for _ in range(K-1):
-        a = lst.pop(0)
-        lst.append(a)
-    b = lst.pop(0)
-    result.append(b)
-
-print('<'+ ', '.join(map(str, result))+'>')
