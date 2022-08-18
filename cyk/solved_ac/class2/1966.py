4ce42fa532f7aef8258742d0323fb7075ff8ad65
cyg <cyellowgreen@gmail.com>
2022-08-18 23:55:01 +0900

    class2++ 1966 프린터큐

---
+++ cyk/solved_ac/class2/1966.py
@@ -0,0 +1,34 @@
+# 프린터 큐
+'''
+3
+1 0
+5
+4 2
+1 2 3 4
+6 0
+1 1 9 1 1 1
+'''
+
+T = int(input())
+for i in range(T):
+    N, M = map(int, input().split())
+    q = list(map(int, input().split()))
+    cnt = 0
+    while len(q):
+        first = q[0]
+        if first == max(q):
+            if M == 0:
+                cnt+=1
+                break
+            else:
+                q.pop(0)
+                cnt += 1
+                M -= 1
+        else:
+            back = q.pop(0)
+            q.append(back)
+            if M == 0:
+                M += (len(q)-1)
+            else:
+                M -= 1
+    print(cnt+1)
