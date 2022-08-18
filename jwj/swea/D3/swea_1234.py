7a1e82f6a6cd320cb78a5a4fc2e8bba271ccb2e4
Lucas0105 <dnjswo105@gmail.com>
2022-08-18 13:10:01 +0900

    add swea 1234

---
+++ jwj/swea/D3/swea_1234.py
@@ -0,0 +1,22 @@
+for tc in range(1, 11):
+    N, strs = input().split()
+    N = int(N)
+    strs = list(strs)
+    strs_len = len(strs)
+
+    i = 0
+    while i < strs_len-1:
+
+        if strs[i] == strs[i+1]:
+            strs.pop(i)
+            strs.pop(i)
+            i -= 1
+        else:
+            i += 1
+
+        if i == -1:
+            i = 0
+
+        strs_len = len(strs)
+
+    print(f'#{tc} {"".join(strs)}')
