de28f2bdd144f1460d33c940f9c615bcf1e48cef
Lucas0105 <dnjswo105@gmail.com>
2022-08-23 15:34:48 +0900

    fix swea 4875

--- jwj/swea/D1-D2/swea_4875.py
+++ jwj/swea/D1-D2/swea_4875.py
@@ -1,44 +1,41 @@
 T = int(input())
 
 
-def dfs(position):
+def dfs(x, y):
+    stack = [(x, y)]
+    visited[x][y] = True
 
-    if miro[position[0]][position[1]] == 3:
-        return 1
-    else:
+    while stack:
+        x, y = stack.pop()
+
         for i in range(4):
-            tmp_x = position[0] + deltaX[i]
-            tmp_y = position[1] + deltaY[i]
+            tmp_x = x + deltaX[i]
+            tmp_y = y + deltaY[i]
 
-            if 0 <= tmp_x < N and 0 <= tmp_y < N and miro[tmp_x][tmp_y] != 1 and not visited[tmp_x][tmp_y]:
+            if 0 <= tmp_x < N and 0 <= tmp_y < N and miro[tmp_x][tmp_y] != '1' and not visited[tmp_x][tmp_y]:
                 visited[tmp_x][tmp_y] = True
-                res = dfs([tmp_x, tmp_y])
-
-                if res == 1:
+                stack.append((tmp_x, tmp_y))
+                if miro[tmp_x][tmp_y] == '3':
                     return 1
+
     return 0
 
 
 for tc in range(1, T+1):
     N = int(input())
-    miro = [list(map(int, input())) for _ in range(N)]
-
-    position = []
+    miro = [list(input()) for _ in range(N)]
 
     deltaX = [-1, 0, 1, 0]
     deltaY = [0, 1, 0, -1]
     visited = [[False] * N for _ in range(N)]
 
-    for x in range(N):
-        for y in range(N):
-            if miro[x][y] == 2:
-                position = [x, y]
-                break
-        if position:
+    for idx in range(N):
+        if '2' in miro[idx]:
+            x, y = idx, miro[idx].index('2')
             break
 
-    visited[position[0]][position[1]] = True
+    visited[x][y] = True
 
-    res = dfs(position)
+    res = dfs(x, y)
 
     print(f'#{tc} {res}')
