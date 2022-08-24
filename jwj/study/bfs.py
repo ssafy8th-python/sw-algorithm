ba325d7a9791eb553c2d9b82f29516ffb27a3f38
Lucas0105 <dnjswo105@gmail.com>
2022-08-24 16:47:32 +0900

    fix bfs 거리측정

--- jwj/study/bfs.py
+++ jwj/study/bfs.py
@@ -23,13 +23,20 @@ visited = [False] * (N+1)
 que = [1]
 visited[1] = True
 
+distance = [0] * (N+1)
+distance[1] = 1
+
 while que:
     v = que.pop(0)
 
     print(v)
 
     for w in graph[v]:
-        if not visited[w]:
+        # if not visited[w]:
+        if not distance[w]:
             visited[w] = True
+            distance[w] = distance[v] + 1   # 거리를 측정할 수 있음
             que.append(w)
 
+
+print(distance[1:])
