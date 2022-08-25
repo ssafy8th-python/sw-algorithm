9d56ea61b35423c10420aeb1ea1e8d9d3245e4f4
appletail <csappletail@gmail.com>
2022-08-25 15:48:57 +0900

    물놀이

--- sjw/swea/D4/10966_물놀이_가자.py
+++ sjw/swea/D4/10966_물놀이_가자.py
@@ -5,22 +5,28 @@ sys.stdin = open("input.txt", "r")
 from collections import deque
 
 
-def bfs(r, c):
-    visited = [[0] * m for _ in range(n)]
+def bfs():
     q = deque()
-    q.append((r, c))
-    visited[r][c] = 1
+    visited = [[-1] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if ground[i][j] == 'W':
+                q.append((i, j))
+                visited[i][j] = 0
     dt = [[1, 0], [-1, 0], [0, 1], [0, -1]]
     while q:
         r, c = q.popleft()
-        if ground[r][c] == 'W':
-            return visited[r][c] - 1
-        for i in dt:
-            nr = r + i[0]
-            nc = c + i[1]
-            if 0 <= nr < n and 0 <= nc < m and not visited[nr][nc]:
+        for i, j in dt:
+            nr = r + i
+            nc = c + j
+            if 0 <= nr < n and 0 <= nc < m and visited[nr][nc] == -1:
                 q.append((nr, nc))
                 visited[nr][nc] = visited[r][c] + 1
+    result = 0
+    for i in visited:
+        result += sum(i)
+
+    return result
 
 
 T = int(input())
@@ -28,13 +34,41 @@ T = int(input())
 for test_case in range(1, T + 1):
     n, m = map(int, input().split())
     ground = [list(input()) for _ in range(n)]
-    result = 0
 
+    print(f'#{test_case}', bfs())
+
+
+from collections import deque
+
+
+def bfs():
+    q = deque()
+    visited = [[0] * m for _ in range(n)]
     for i in range(n):
         for j in range(m):
-            if ground[i][j] == 'L':
-                result += bfs(i, j)
+            if ground[i][j] == 'W':
+                q.append((i, j))
+                visited[i][j] = 0
+    dt = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    while q:
+        r, c = q.popleft()
+        for i in dt:
+            nr = r + i[0]
+            nc = c + i[1]
+            if 0 <= nr < n and 0 <= nc < m and not visited[nr][nc] and ground[nr][nc] != 'W':
+                q.append((nr, nc))
+                visited[nr][nc] = visited[r][c] + 1
+    result = 0
+    for i in visited:
+        result += sum(i)
 
-    print(f'#{test_case}', result)
+    return result
 
 
+T = int(input())
+
+for test_case in range(1, T + 1):
+    n, m = map(int, input().split())
+    ground = [list(input()) for _ in range(n)]
+
+    print(f'#{test_case}', bfs())
