88459909a1a8eda4f5da66adbf3421d5ddbc7a0e
appletail <csappletail@gmail.com>
2022-08-28 22:00:16 +0900

    test

--- sjw/baek/no_class/10157_자리배정.py
+++ sjw/baek/no_class/10157_자리배정.py
@@ -5,3 +5,30 @@
 # import sys
 # input = sys.stdin.readline
 
+x, y = map(int, input().split())
+target = int(input())
+visited = [[True] * x for _ in range(y)]
+
+     # 상 우 하 좌
+dt = [(-1, 0, 0, 1), (0, 1, 1, 0), (1, 0, 0, -1), (0, -1, -1, 0)]
+
+cnt = 1
+idx = 0
+r, c = y - 1, 0
+visited[r][c] = False
+result = [1, 1]
+while cnt < x * y and cnt != target:
+    nr = r + dt[idx][0]
+    nc = c + dt[idx][1]
+    if 0 <= nr < y and 0 <= nc < x and visited[nr][nc]:
+        cnt += 1
+        visited[nr][nc] = False
+        r, c = nr, nc
+        result = [result[0] + dt[idx][2], result[1] + dt[idx][3]]
+    else:
+        idx = (idx + 1) % 4
+
+if cnt < target:
+    print(0)
+else:
+    print(*result)
