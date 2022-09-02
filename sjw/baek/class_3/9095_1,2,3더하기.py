0d8501bdba3c0fdf08c870d5ed0c7f92d58efa67
appletail <csappletail@gmail.com>
2022-09-02 15:12:50 +0900

    백준 1문제

---
+++ sjw/baek/class_3/9095_1,2,3더하기.py
@@ -0,0 +1,19 @@
+# 9095 1,2,3 더하기
+# 주소: https://www.acmicpc.net/problem/9095
+
+def check(k):
+    if lst[k] != 0:
+        return lst[k]
+    else:
+        lst[k] = check(k - 1) + check(k - 2) + check(k - 3)
+        return lst[k]
+
+
+t = int(input())
+
+lst = [0, 1, 2, 4] + [0] * 8
+
+for _ in range(t):
+    n = int(input())
+
+    print(check(n))
