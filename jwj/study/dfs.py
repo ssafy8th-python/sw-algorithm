e0775b3a33d690148615aaab46e195280d1309c9
Lucas0105 <dnjswo105@gmail.com>
2022-09-12 08:44:33 +0900

    update dfs algorithm

--- jwj/study/dfs.py
+++ jwj/study/dfs.py
@@ -1,29 +1,31 @@
 '''
     정점 간선
 7 8
-0 1
-0 2
+1 2
 1 3
-1 4
 2 4
-3 5
-4 5
+2 5
+4 6
 5 6
+6 7
+3 7
 '''
 
 
 '''
 stack을 이용해 백트래킹 하는 방법
 '''
-# def dfs(v):
-#     stack = []
-#     visited[v] = True
+
 #
-#     while True:
+# def dfs(v):
+#     stack = [v]
+#     visited[v] = True
+#     print(v)
+#     while stack:
 #
 #         for w in graph[v]:
 #             if not visited[w]:
-#                 print(w+1)
+#                 print(w)
 #                 stack.append(v)
 #                 v = w
 #                 visited[v] = True
@@ -31,8 +33,20 @@ stack을 이용해 백트래킹 하는 방법
 #         else:
 #             v = stack.pop()
 #
-#         if not stack:
-#             break
+#
+# N, M = map(int, input().split())        # 정점, 간선
+#
+# graph = [[] for _ in range(N + 1)]          # 정점의 수 만큼 생성
+#
+#
+# for _ in range(M):                      # 간선 수 만큼 진행
+#     x, y = map(int, input().split())
+#     graph[x].append(y)
+#     graph[y].append(x)
+#
+#
+# visited = [False] * (N + 1)
+# dfs(1)
 
 '''
 stack을 이용해 방문하지 않은 곳 방문 하기
@@ -52,9 +66,11 @@ stack을 이용해 방문하지 않은 곳 방문 하기
 '''
 재귀를 이용 하는 방법
 '''
+
+
 def dfs(v):
     visited[v] = True
-    print(v+1)
+    print(v)
     for w in graph[v]:
         if not visited[w]:
             dfs(w)
@@ -62,7 +78,7 @@ def dfs(v):
 
 N, M = map(int, input().split())        # 정점, 간선
 
-graph = [[] for _ in range(N)]          # 정점의 수 만큼 생성
+graph = [[] for _ in range(N + 1)]          # 정점의 수 만큼 생성
 
 
 for _ in range(M):                      # 간선 수 만큼 진행
@@ -70,6 +86,6 @@ for _ in range(M):                      # 간선 수 만큼 진행
     graph[x].append(y)
     graph[y].append(x)
 
-visited = [False] * N
+visited = [False] * (N + 1)
 
-dfs(0)
+dfs(1)
