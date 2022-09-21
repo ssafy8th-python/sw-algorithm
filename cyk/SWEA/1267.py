7ad9925395bf56fa57966c28801e96631eb90d14
cygyeong <cyellowgreen@gmail.com>
2022-09-21 10:55:57 +0900

    SWEA 1267 작업순서

--- cyk/SWEA/1267.py
+++ cyk/SWEA/1267.py
@@ -1,40 +1,30 @@
 # 작업 순서
 # 방향성을 가진 간선
-# import sys
-# sys.stdin = open("sample_input (7).txt", "r")
+import sys
+sys.stdin = open("sample_input.txt", "r")
+def order(arr):
+    while any(arr):
+        for idx in range(1, len(arr)):
+            if not arr[idx] and not idx in res:  # 비어있다면
+                res.append(idx)
+                for i in range(1, len(arr)):
+                    if idx in arr[i]:
+                        find_idx = arr[i].index(idx)
+                        arr[i].pop(find_idx)
 
-def order(start_lst):
-    st = []
-    for elem in start_lst:
-        st.append(elem)
-    start = st.pop()
-    print(start, end=' ')
-    start = st.pop()
-    while st:
-        for w in lst[start]:
-            if not visited[w]:
-                print(w, end=' ')
-                st.append(start)
-                start = w
-                visited[start] = True
-                break
-        else:
-            start = st.pop()
-
-for tc in range(1, 2):
+for tc in range(1, 11):
     V, E = map(int, input().split())
-    lst = [[] for _ in range(V+1)]
+    arr = [[] for _ in range(V + 1)]
     ipt = list(map(int, input().split()))
     start = ipt[0]
-    start_lst = []
 
-    for i in range(0, E*2, 2):
-        lst[ipt[i+1]].append(ipt[i])
-        start_lst.append(ipt[i+1])
-    visited = [False] * (len(lst))
-    print(lst)
-    start_lst = list(set(list(range(1,E+1)))-set(start_lst))
-    print(start_lst)
-    order(start_lst)
+    for i in range(0, E * 2, 2):
+        arr[ipt[i + 1]].append(ipt[i])
+    res = []
+    order(arr)
+    for i in range(1, len(arr)):
+        if not i in res:
+            res.append(i)
 
-    print()
+    print(f'#{tc}', end=' ')
+    print(*res)
