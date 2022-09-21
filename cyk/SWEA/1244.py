1442b28ef8210285c363affcf6955bf910645765
cygyeong <cyellowgreen@gmail.com>
2022-09-22 00:00:41 +0900

    SWEA 1244 최대상금미완

---
+++ cyk/SWEA/1244.py
@@ -0,0 +1,17 @@
+# 최대상금
+# 최대 여섯자리수, 최대 10번의 교환횟수
+'''
+1
+123 1
+'''
+def f(k):
+    if k == count:
+        return
+
+
+T = int(input())
+for tc in range(1, 1+T):
+    num_lst, count = input().split()
+    num_lst, count= list(map(int, list(num_lst))), int(count)
+    size = len(num_lst)
+    f(1)git
