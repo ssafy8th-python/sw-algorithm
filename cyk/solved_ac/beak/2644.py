902db2bcd66c5bbe07fe623a0c564cd303401f49
cygyeong <cyellowgreen@gmail.com>
2022-09-21 09:02:25 +0900

    beak 2644 촌수계산

--- cyk/solved_ac/beak/2644.py
+++ cyk/solved_ac/beak/2644.py
@@ -1,3 +1,4 @@
+# 촌수 계산
 n = int(input())
 num1, num2 = map(int, input().split())
 m = int(input())
@@ -6,15 +7,16 @@ arr = [[] for _ in range(n+1)]
 def bfs(start, end):
     q = [start]
     visited = [0] * (n+1)
+    visited[start] = 1
+
     while q:
         x = q.pop(0)
-        visited[x] = 1
         for w in arr[x]:
             if not visited[w]:
                 visited[w] = visited[x]+1
                 q.append(w)
                 if w == end:
-                    return visited[w]+1
+                    return visited[w]-1
     return -1
 
 for _ in range(m):
